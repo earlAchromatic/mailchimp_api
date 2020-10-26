70ce7f316f8a5944aba6f78a7a5524a618f77845
earlAchromatic <68669571+earlAchromatic@users.noreply.github.com>
2020-10-26 15:02:59 -0400

    test form

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,15 +1,12 @@
 
 
 const fetch = require('node-fetch')
-const mailchimp = require('@mailchimp/mailchimp_marketing');
+const base64 = require('base-64')
 
-
-const MC_url = 'https://server.api.mailchimp.com/3.0'
-
-const MC_API = '7833d0810f0c5f1d16f9267e35d8a318-us2'
-const MC_server = 'us2'
-const Audience_ID = '3e38748fa4'
-
+// const MC_url = 'https://server.api.mailchimp.com/3.0'
+// const MC_API = '7833d0810f0c5f1d16f9267e35d8a318-us2'
+// const MC_server = 'us2'
+// const LIST_ID = '3e38748fa4'
 //const getList = `/lists/${Audience_ID}`
 
 exports.handler = async (event) =>{
@@ -19,25 +16,54 @@ exports.handler = async (event) =>{
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
-  
-  mailchimp.setConfig({
-    apiKey: MC_API,
-    server: MC_server,
-  });
-
-
-  const run = async () => {
-    try{
-      const response = await client.lists.getList(Audience_ID);
-      console.log(response);
-      return { statusCode: 200, body: response.body };
-    }catch(err){
-      return { statusCode: 400, body: err.body };
-    }
-    
+  const errorGen = msg => {
+    return { statusCode: 500, body: msg }; 
   };
+
+  try{
+    const {email} = JSON.parse(event.body);
+
+    if(!email){
+      return errorGen('Missing Email')
+    }
+
+    const subscriber = {
+      email_address: email,
+      status: 'subscribed'
+    }
+
+    const credentials = `any: ${process.env.MC_API}`
+
+    const response = await
+    fetch(`https://${MC_SERVER}.api.mailchimp.com/3.0/lists/${LIST_ID}/members/`, {
+      method: 'POST',
+      headers: { 
+        Accept: '*/*', 
+        'Content-Type': 'application/json', 
+        Authorization: `Basic ${base64.encode(credentials)}`},
+        body: JSON.stringify(subscriber)
+    });
+
+    const data = await response.JSON();
+
+    if (!response.ok){
+      return {statusCode: data.status, body: data.detail}
+    }
+
+    return {statusCode: 200, 
+      body: JSON.stringify({msg: "You've signed up to the mailing list!", detail: data})
+    };
+    
+  }
+  catch(err){
+    console.log(err);
+    return { 
+      statusCode: 500, 
+      body: JSON.stringify({ msg: err.message }),
+    };
+  }
   
-  run()
+
 
 
 }
